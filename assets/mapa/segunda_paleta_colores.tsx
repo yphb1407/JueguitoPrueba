b32eedd3252b43bb77067d9640e89fad5b74e46c
Heberblabla <132624731+Heberblabla@users.noreply.github.com>
2026-07-11 19:37:39 -0500

    Add files via upload

---
+++ assets/mapa/segunda_paleta_colores.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="segunda_paleta_colores" tilewidth="100" tileheight="100" tilecount="100" columns="10">
+ <image source="segunda_paleta_colores.png" width="1000" height="1000"/>
+</tileset>
